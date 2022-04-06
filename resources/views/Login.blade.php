7aeb2d29230110862990456962dc62535fc0ba8b
Trinh Minh Quang <quangtmbhaf200029.fpt.edu.vn>
2022-04-06 21:05:09 +0700

    quang sửa pass

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -12,7 +12,7 @@
           </div>
           <div class="input-box-r">
             <span class="details">Password</span>
-            <input type="text" placeholder="Enter your password" required>
+            <input type="password" placeholder="Enter your password" required>
           </div>
         </div>
         <div class="button">
