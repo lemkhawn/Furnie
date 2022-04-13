45fb6cbc63da18de799162211d3032bbef1bd736
lemkhawn <khanhnlbhbf200025@fpt.edu.vn>
2022-04-13 15:19:49 +0700

    LK update

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 // use Session;
 use App\Models\User;
-use Auth;
+// use Auth;
 
 
 class loginController extends Controller
